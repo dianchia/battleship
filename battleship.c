19e5ebfec297418fcabe834df2b2827d0a5996d0
dianchia <32894279+dianchia@users.noreply.github.com>
2017-10-28 18:03:16 +0800

    Update battleship.c

--- battleship.c
+++ battleship.c
@@ -1,12 +1,42 @@
 #include <stdio.h>
-#include <battleship.h>
+# include <battleship.h>
+#include <stdlib.h>
 
-int diff;
+int diff, i;
+char retry[1];
+int AskRetry;
 
-int main (void){
-	WelcomeMsg();
-	printf("\nChoose your difficulty: (1-Beginner. 2-Intermediate, 3-Advance)");
-	scanf("%d", &diff);
-	SetShip(diff);
-	
+int main(void){
+	do{
+		Initialize();		
+		WelcomeMsg();
+		printf("Please choose a difficulty: (1-beginner, 2-intermediate, 3-advance)");
+		scanf("%d", &diff);
+		SetShip(diff);
+		int bomb_count = 15;
+		while(bomb_count > 0){
+			PrintMaps();
+			PrintShip();
+			printf("\n\nYou still have %d bomb left.", bomb_count);
+			CheckMap();
+			bomb_count--;
+			printf("You still have %d bomb left.", bomb_count);
+			system("cls");
+			AskRetry = CheckScore();
+			if(AskRetry == 1){
+				break;
+			}
+		}
+		if(AskRetry == 1){
+			printf("Do you wish to retry? (Y or N)");
+			scanf("%1s", retry);
+		}
+		else{
+			printf("\nSorry, you have ran out of bomb. Try again? (Y or N) ");
+			scanf("%1s", retry);
+		}
+
+	}while(retry[0] == 'Y' || retry[0] == 'y');
+	printf("\nThank You for playing this game! Hope to see you again soon.");
+	return 0;
 }
